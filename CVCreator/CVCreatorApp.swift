fc19325c49f7c7f7bbc3ad5507fa304d0ac1189a
Nathan <nathan.chicha.openclassroom@gmail.com>
2021-10-21 16:08:02 +0200

    Initial Commit

---
+++ CVCreator/CVCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVCreatorApp.swift
+//  CVCreator
+//
+//  Created by Nathan on 21/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct CVCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
